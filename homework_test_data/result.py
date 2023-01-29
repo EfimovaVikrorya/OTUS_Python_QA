29e42fb1b1cff8d00ce0df5cfbba5a547dbc5e68
EfimovaVikroria <vilka.ru@mail.ru>
2023-01-29 18:21:29 +0300

    homework_test_data

--- homework_test_data/result.py
+++ homework_test_data/result.py
@@ -1,6 +1,6 @@
 import csv
 import json
-from csv import  DictReader
+from csv import DictReader
 from files import CSV_FILE_PATH
 from files import JSON_FILE_PATH
 
@@ -10,14 +10,16 @@ with open(JSON_FILE_PATH, "r") as users:
     users_reader = json.loads(users.read())
 
     for user in users_reader:
-        result.append({'name':user['name'],'gender':user['gender'],'address':user['address'],'age':user['age'], 'books':[]})
+        result.append({'name': user['name'], 'gender': user['gender'], 'address': user['address'], 'age': user['age'],
+                       'books': []})
 
 index = 0
 with open(CSV_FILE_PATH, newline='') as books:
     list_books = DictReader(books)
 
     for book in list_books:
-        result[index]['books'].append({'title':book['Title'],'author':book['Author'],'pages':book['Pages'],'genre':book['Genre']})
+        result[index]['books'].append(
+            {'title': book['Title'], 'author': book['Author'], 'pages': book['Pages'], 'genre': book['Genre']})
         index += 1
         if index >= len(result):
             index = 0
@@ -25,4 +27,3 @@ with open(CSV_FILE_PATH, newline='') as books:
 with open("result.json", "w") as f:
     s = json.dumps(result, indent=4)
     f.write(s)
-
